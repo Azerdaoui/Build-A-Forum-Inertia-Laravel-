c961a38fc1d2c7cc1ab9e456df42f9455478ef03
Othmane AZERDAOUI <oazerdaoui@gear9.ma>
2023-06-09 12:39:10 +0100

    Creatre ForumLayout componenet

--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,24 +1,26 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import ForumLayout from '@/Layouts/ForumLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Forum" />
 
-    <AuthenticatedLayout>
+    <ForumLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-
-                    </div>
-                </div>
+        <div class="sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900"></div>
             </div>
         </div>
-    </AuthenticatedLayout>
+
+        <template #side>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">Side</div>
+            </div>
+        </template>
+    </ForumLayout>
 </template>
